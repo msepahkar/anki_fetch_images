753f1f212dc577de7483cf4e9c87689196455f70
mehdi <msepahkar@gmail.com>
2017-01-30 17:49:25 +0330

    working on item delegate started

--- test4.py
+++ test4.py
@@ -1,50 +1,74 @@
-from PyQt4.QtGui import *
-from PyQt4.QtCore import *
-from PyQt4.QtNetwork import *
 import sys
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
 
-
-# Subclass QNetworkAccessManager Here
-class NetworkAccessManager(QNetworkAccessManager):
-    # Save image data in QByteArray buffer to the disk (google_image_logo.png
-    # in the same directory)
-    @pyqtSlot()
-    def slotFinished(self):
-        print 'finished'
-        self.messageBuffer += self.reply.readAll()
-        imageFile = QFile("/home/mehdi/google_image_logo.png")
-        if (imageFile.open(QIODevice.WriteOnly)):
-            imageFile.write(self.messageBuffer)
-            imageFile.close()
-            QMessageBox.information(None, "Hello!", "File has been saved!")
-        else:
-            QMessageBox.critical(None, "Hello!", "Error saving file!")
-
-    # Append current data to the buffer every time readyRead() signal is emitted
-    @pyqtSlot()
-    def slotReadData(self):
-        print 'new data'
-        self.messageBuffer += self.reply.readAll()
-
-    def __init__(self):
-        QNetworkAccessManager.__init__(self)
-        self.messageBuffer = QByteArray()
-        url = QUrl("http://upload.wikimedia.org/wikipedia/commons/f/fe/Google_Images_Logo.png")
-        req = QNetworkRequest(url)
-        self.reply = self.get(req)
-
-        QObject.connect(self.reply, SIGNAL("readyRead()"), self, SLOT("slotReadData()"))
-        QObject.connect(self.reply, SIGNAL("finished()"), self, SLOT("slotFinished()"))
-        # End of NetworkAccessManager Class
-
-
-###################################
-
+####################################################################
 def main():
     app = QApplication(sys.argv)
-    networkAccessManager = NetworkAccessManager()
+    w = MyWindow()
+    w.show()
     sys.exit(app.exec_())
 
+####################################################################
+class MyWindow(QWidget):
+    def __init__(self, *args):
+        QWidget.__init__(self, *args)
 
-if __name__ == '__main__':
+        # create objects
+        list_data = [1,2,3,4]
+        lm = MyListModel(list_data, self)
+        de = MyDelegate(self)
+        lv = QListView()
+        lv.setModel(lm)
+        lv.setItemDelegate(de)
+
+        # layout
+        layout = QVBoxLayout()
+        layout.addWidget(lv)
+        self.setLayout(layout)
+
+####################################################################
+class MyDelegate(QItemDelegate):
+    def __init__(self, parent=None, *args):
+        QItemDelegate.__init__(self, parent, *args)
+
+    def paint(self, painter, option, index):
+        painter.save()
+
+        # set background color
+        painter.setPen(QPen(Qt.NoPen))
+        if option.state & QStyle.State_Selected:
+            painter.setBrush(QBrush(Qt.red))
+        else:
+            painter.setBrush(QBrush(Qt.white))
+        painter.drawRect(option.rect)
+
+        # set text color
+        painter.setPen(QPen(Qt.black))
+        value = index.data(Qt.DisplayRole)
+        if value.isValid():
+            text = value.toString()
+            painter.drawText(option.rect, Qt.AlignLeft, text)
+
+        painter.restore()
+
+####################################################################
+class MyListModel(QAbstractListModel):
+    def __init__(self, datain, parent=None, *args):
+        """ datain: a list where each item is a row
+        """
+        QAbstractTableModel.__init__(self, parent, *args)
+        self.listdata = datain
+
+    def rowCount(self, parent=QModelIndex()):
+        return len(self.listdata)
+
+    def data(self, index, role):
+        if index.isValid() and role == Qt.DisplayRole:
+            return QVariant(self.listdata[index.row()])
+        else:
+            return QVariant()
+
+####################################################################
+if __name__ == "__main__":
     main()
